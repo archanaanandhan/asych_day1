a1a3cc1df2da5cd4b01f548363c77cee99fc5229
archanaanandhan <141989161+archanaanandhan@users.noreply.github.com>
2023-11-27 12:09:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById("cb_hall").innerText="10";
+    setTimeout(() => {
+        document.getElementById("cb_hall").innerText="9";
+        setTimeout(() => {
+            document.getElementById("cb_hall").innerText="8";
+            setTimeout(() => {
+                document.getElementById("cb_hall").innerText="7";
+                setTimeout(() => {
+                    document.getElementById("cb_hall").innerText="6";
+                    setTimeout(() => {
+                        document.getElementById("cb_hall").innerText="5";
+                        setTimeout(() => {
+                            document.getElementById("cb_hall").innerText="4";
+                            setTimeout(() => {
+                                document.getElementById("cb_hall").innerText="3";
+                                setTimeout(() => {
+                                    document.getElementById("cb_hall").innerText="2";
+                                    setTimeout(() => {
+                                        document.getElementById("cb_hall").innerText="1";
+                                        setTimeout(() => {
+                                            document.getElementById("cb_hall").innerText="happy independence day";
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
